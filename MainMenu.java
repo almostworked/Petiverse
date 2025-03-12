c2cca13cfa28d01a6b0669120d531a3d9ab7608d
Daniella Misyura <dmisyura@uwo.ca>
2025-03-12 16:48:36 -0400

    Added init main + other classes + game assets and fonts

---
+++ MainMenu.java
@@ -0,0 +1,33 @@
+public class MainMenu {
+    public static void main(String args[]) {
+
+    }
+    
+    public int displayStats() {
+        return 0;
+    }
+
+    public void displayInstructions() {
+
+    }
+
+    public void parentalControls() {
+
+    }
+
+    public void playNewGame() {
+
+    }
+
+    public void loadGame(String filename) {
+
+    }
+
+    public void displaySettings() {
+
+    }
+
+    public void exit() {
+
+    }
+}
---
+++ PlayGame.java
@@ -0,0 +1,6 @@
+public interface PlayGame {
+    public void displayPetState();
+    public void displayAllCommands();
+    public void displayGiftCommands();
+    public void back();
+}
