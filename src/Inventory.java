e92cc85aefbe8ee0d407096e2b9d80f965e89eb7
Fiyinfoluwa Oluwagbemiga Faniyi <ffaniyi@uwo.ca>
2025-03-18 23:21:23 +0000

    Update Inventory.java

--- src/Inventory.java
+++ src/Inventory.java
@@ -5,14 +5,14 @@ import java.util.List;
  * 
  * @author faniyi
  * The {@code Inventory } class is to manage a collection of items
- * which can add, remove and update items in invetory
+ * which can add, remove and update items in inventory
  */
 public class Inventory {
         /**
          * A list to store the inventory items
          * A flag to check if the inventory contains gifts
          */
-	private List<Item> item; 
+	private List<Item> items; 
 	private boolean hasGifts;
 	
         /**
@@ -26,7 +26,7 @@ public class Inventory {
 	/**
          * A constructor that initializes the inventory with existing items
          * @param items A list of items to initialize the inventory
-         * @param hasGifts A boolean indicated if the inventory contains gifts
+         * @param hasGifts A boolean indicates if the inventory contains gifts
          */
 	public Inventory(List<Item> items, boolean hasGifts) {
         this.items = new ArrayList<>(items);
@@ -34,7 +34,7 @@ public class Inventory {
     }
 	
         /**
-         * This retrives the quantity of an item in the inventory
+         * This retrieves the quantity of an item in the inventory
          * @param itemName The name of the item 
          * @return The amount of the specified item or 0 if it is not found
          */
@@ -80,13 +80,13 @@ public class Inventory {
 	public void displayMethods() {
 		System.out.println("Here are the available inventory methods:");
         System.out.println("1. You can display inventory");
-        System.out.println("2. You can refreshUpdate Inventory");
+        System.out.println("2. You can Update Inventory");
         System.out.println("3. Remove Item");
 	}
 	
         /**
          * Updates the inventory by adding an amount of an item
-         * If it exits, it will be increased else it will be created
+         * If it exists, it will be increased else it will be created
          * @param itemName The name of the item to update 
          * @param quantity The amount of item to add
          */
@@ -104,8 +104,8 @@ public class Inventory {
         /**
          * Removes a specified amount of an item from the inventory
          * If the quantity is zero or negative then it will be removed
-         * @param itemName
-         * @param quantity 
+         * @param itemName The name of the item to remove
+         * @param quantity The quantity to remove
          */
 	public void removeItem(String itemName, int quantity) {
 		for (int i = 0; i < items.size(); i++) {
