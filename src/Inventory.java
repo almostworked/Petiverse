719bf55b2038a28e878bebe68db28f5254eb66b6
Fiyinfoluwa Oluwagbemiga Faniyi <ffaniyi@uwo.ca>
2025-03-18 22:56:09 +0000

    Added Java Doc and added a SetQuantity method

--- src/Inventory.java
+++ src/Inventory.java
@@ -1,33 +1,67 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * @author faniyi
+ * The {@code Inventory } class is to manage a collection of items
+ * which can add, remove and update items in invetory
+ */
 public class Inventory {
-	private List<Item> item; //Made a list to store the items
+        /**
+         * A list to store the inventory items
+         * A flag to check if the inventory contains gifts
+         */
+	private List<Item> item; 
 	private boolean hasGifts;
 	
-	//2 constructors
-	//empty constructor
+        /**
+         * default constructor that initializes an empty inventory
+         */
 	public Inventory() {
         this.items = new ArrayList<>();
         this.hasGifts = false;
     }
 	
-	//Constructor with existing items
+	/**
+         * A constructor that initializes the inventory with existing items
+         * @param items A list of items to initialize the inventory
+         * @param hasGifts A boolean indicated if the inventory contains gifts
+         */
 	public Inventory(List<Item> items, boolean hasGifts) {
         this.items = new ArrayList<>(items);
         this.hasGifts = hasGifts;
     }
 	
+        /**
+         * This retrives the quantity of an item in the inventory
+         * @param itemName The name of the item 
+         * @return The amount of the specified item or 0 if it is not found
+         */
+        
 	public int getQuantity(String itemName) {
 		for (Item item : items) {
 			if (item.getName().equalsIgnoreCase(itemName)) {
 				return item.getQuantity();
 			}
 		}
-		return 0; //This returns 0 if item is not found
+		return 0; //This returns 0 if an item is not found
 	}
 	
-	//Display inventory contents
+        public void setQuantity(String itemName, int quantity){
+            for (Item item : items){
+                if (item.getName().equalsIgnoreCase(itemName)){
+                    item.setQuantity(quantity);
+                    return;
+                }
+            }
+            // And if the item does not exist, we will add a quantity
+            items.add(new Item(itemName, quantity));
+        }
+	/**
+         * Display the inventory contents
+         * @return the current inventory and the items in it
+         */
 	public Inventory displayInventory() {
 		System.out.println("Inventory:");
 		if (items.isEmpty()) {
@@ -40,6 +74,9 @@ public class Inventory {
 		return this;
 	}
 	
+        /**
+         * Displays the available inventory methods 
+         */
 	public void displayMethods() {
 		System.out.println("Here are the available inventory methods:");
         System.out.println("1. You can display inventory");
@@ -47,6 +84,12 @@ public class Inventory {
         System.out.println("3. Remove Item");
 	}
 	
+        /**
+         * Updates the inventory by adding an amount of an item
+         * If it exits, it will be increased else it will be created
+         * @param itemName The name of the item to update 
+         * @param quantity The amount of item to add
+         */
 	public void updateInventory (String itemName, int quantity) {
 		for (Item item : items) {
             if (item.getName().equalsIgnoreCase(itemName)) {
@@ -58,6 +101,12 @@ public class Inventory {
         items.add(new Item(itemName, quantity));
 	}
 	
+        /**
+         * Removes a specified amount of an item from the inventory
+         * If the quantity is zero or negative then it will be removed
+         * @param itemName
+         * @param quantity 
+         */
 	public void removeItem(String itemName, int quantity) {
 		for (int i = 0; i < items.size(); i++) {
             Item item = items.get(i);
