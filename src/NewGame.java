50162cae1b521b450eba8ed4ae0b33d606a3fb0e
Marcus Jacob Cameron <mcamer94@uwo.ca>
2025-03-23 17:41:51 +0000

    Added Javadocs to class NewGame

--- src/NewGame.java
+++ src/NewGame.java
@@ -1,4 +1,4 @@
-/**
+package src; /**
  * When a new game is selected, the player is presented with a choice of picking from at least 3 different virtual pet types.
  * An image representing each pet type should be displayed on the screen and well as some basic information about each pet type.
  * The user should be able to select one of the pets and give it a name.
@@ -7,16 +7,30 @@
 
 // note to self - players should be able to give the pet a nickname
 // have to display info about each prt type
+import src.Pet;
+
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * The purpose of this class is to create a new game and initialize pet statistics such as
+ * hunger, happiness, health and sleep
+ */
 public class NewGame {
+    /** attributes */
     private String petName;
     private String playerName;
     private int saveSlot;
     private Pet selectedPet;
 
-    // Constructor
+    /**
+     * The constructor method
+     * Initializes and saves a new game, assigning all statistics the value of 100
+     *
+     * @param playerName is the name of the player (stored using saveGame() method)
+     * @param saveSlot is the save slot integer where the game will be stored
+     * @param selectedPet is the Pet the player has chosen
+     */
     public NewGame(String playerName, int saveSlot, Pet selectedPet) {
         this.playerName = playerName;
         this.saveSlot = saveSlot;
@@ -30,6 +44,11 @@ public class NewGame {
         saveGame();
     }
 
+    /**
+     * This method attempts to append the game information (player & pet data) to a csv file
+     * If there is an issue in saving the game, an IOException is thrown
+     *
+     */
     public void saveGame() {
         try (FileWriter writer = new FileWriter("saved_games.csv", true)) {
             writer.write(saveSlot + "," + playerName + "," + petName + ", 100, 100, 100, 100\n");
@@ -38,18 +57,30 @@ public class NewGame {
         }
     }
 
+    /**
+     * @return the selected pet of type Pet
+     */
     public Pet getSelectedPet() {
         return this.selectedPet;
     }
 
+    /**
+     * @return the name of the pet
+     */
     public String getPetName() {
         return this.petName;
     }
 
+    /**
+     * @return the name of the player
+     */
     public String getPlayerName() {
         return this.playerName;
     }
 
+    /**
+     * @return the save slot integer
+     */
     public int getSaveSlot() {
         return this.saveSlot;
     }
