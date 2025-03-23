54dddc71c0dbf00d0ec36d81a7ea26a0a347ca3a
Marcus Jacob Cameron <mcamer94@uwo.ca>
2025-03-23 17:41:00 +0000

    Added Javadocs to class LoadGame

--- src/LoadGame.java
+++ src/LoadGame.java
@@ -1,8 +1,22 @@
+package src;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LoadGame {
+/**
+ * The purpose of this class is to parse the csv file containing saved games
+ * and load the selected game
+ *
+ * @author marcus
+ */
+ class LoadGame {
+
+    /**
+     * This method parses through the game_save.csv file, scanning slot number, player name and pet name
+     *
+     * @return savedGames, a list of saved games
+     */
     public List<String> loadSavedGames() {
         List<String> savedGames = new ArrayList<String>();
         try (BufferedReader reader = new BufferedReader(new FileReader("game_save.csv"))) {
@@ -12,11 +26,17 @@ public class LoadGame {
                 if (data.length >= 3) savedGames.add("Slot " + data[0] + ": " + data[1] + "'s pet " + data[2]);
             }
         } catch (IOException e) {
-            System.out.println("An error occured");
+            System.out.println("An error occurred");
         }
 
         return savedGames;
     }
+
+    /**
+     * This method loads and opens the game based on the slot number passed as a parameter
+     *
+     * @param slotNumber represent the integer game slot to be loaded
+     */
     public void loadGame(int slotNumber) {
         // have to load and open the game!
         // doesnt load anything rn
