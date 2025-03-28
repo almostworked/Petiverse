6919e0bca313a127f9c3b1a0d6de83eec6f9652f
Marcus Jacob Cameron <mcamer94@uwo.ca>
2025-03-28 20:37:52 +0000

    Update Sound.java

--- src/Sound.java
+++ src/Sound.java
@@ -8,19 +8,9 @@ import javax.sound.sampled.*;
  */
 public class Sound {
     /** attributes */
-    private Clip clip;
+    private static Clip clip;
     private long clipTimePosition = 0;
-    private boolean isPaused = false;
-
-    /**
-     * Initialize and play a given audio file
-     *
-     */
-    public static void main(String[] args) {
-        // ADD THESE 2 LINES OF CODE TO THE MAIN MENU TO GET SOUND 
-        Sound sound = new Sound();
-        sound.playMusic("temp_assets/background_music.wav");
-    }
+    private static boolean isSoundEnabled = false;
 
     /**
      * Given a filepath passed as parameter, loop and play tue audio recording
@@ -28,44 +18,20 @@ public class Sound {
      *
      * @param location is the filepath of the audio recording
      */
-    public void playMusic(String location) {
+    public static void playMusic(String location) {
+        if (!isSoundEnabled) return;
         try {
             File path = new File(location);
             if (path.exists()) {
                 AudioInputStream audioInput = AudioSystem.getAudioInputStream(path);
-
+                
                 clip = AudioSystem.getClip();
                 clip.open(audioInput);
                 clip.start();
                 // Continuously loop the audio clip
                 clip.loop(Clip.LOOP_CONTINUOUSLY);
-
-                System.out.println("Music is playing. Enter the 'P' key to pause or the 'R' key to resume");
-
-                // Parse any user input
-                new Thread(() -> {
-                    Scanner scanner = new Scanner(System.in);
-                    while (true) {
-                        String input = scanner.nextLine();
-                        // Click P to pause
-                        if (input.equalsIgnoreCase("P")) {
-                            pauseMusic();
-                            // Click R to resume
-                        } else if (input.equalsIgnoreCase("R")) {
-                            resumeMusic();
-                            break;
-                        }
-                    }
-                    scanner.close();
-                }).start();
-            } else {
-                System.out.println("File not found");
             }
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (LineUnavailableException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -75,52 +41,43 @@ public class Sound {
      *
      * @param location is the filepath of the audio recording
      */
-    public void playEffect(String location) {
+    public static void playEffect(String location) {
+        if (!isSoundEnabled) return;
         try {
             File path = new File(location);
             if (path.exists()) {
                 AudioInputStream audioInput = AudioSystem.getAudioInputStream(path);
-
-                clip = AudioSystem.getClip();
+                Clip clip = AudioSystem.getClip();
                 clip.open(audioInput);
                 clip.start();
-
-            } else {
-                System.out.println("File not found");
             }
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (LineUnavailableException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     /**
-     * Method to pause audio
-     */
-    public void pauseMusic() {
-        if (clip != null && clip.isRunning()) {
-            isPaused = true;
+    * Method to track if sound is enabled
+    *
+    */
+    public static boolean isSoundEnabled() {
+        return isSoundEnabled;
+    }
 
-            clipTimePosition = clip.getMicrosecondPosition();
+    /**
+    * Method to toggle sound between on and off
+    *
+    */
+    public static void toggleSound() {
+        // Changes current sound state to it's opposite
+        isSoundEnabled = !isSoundEnabled;
+        if (!isSoundEnabled && clip != null && clip.isRunning()) {
             clip.stop();
+        } else if (isSoundEnabled) {
+            playMusic("temp_assets/background_music.wav");
         }
     }
 
-    /**
-     * Method to resume paused audio
-     */
-    public void resumeMusic() {
-        if (clip != null && isPaused) {
-            isPaused = false;
-
-            clip.setMicrosecondPosition(clipTimePosition);
-            clip.start();
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-        }
-    }
 
 }
 
